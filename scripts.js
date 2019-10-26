fd714322aa78513dff79c8d18a013f1aba32229f
Hector DelaRosa <hdelaros@gmail.com>
2019-10-26 15:07:37 -0400

    updates

---
+++ scripts.js
@@ -0,0 +1,69 @@
+let arrOfUsers = [];
+for (let i = 0; i < 25; i++) {
+    const user = faker.helpers.createCard();
+  user.isFavorite = false;
+  arrOfUsers.push(user);
+}
+
+class List extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      users: arrOfUsers,
+      favUsers: 0,
+    };
+  }
+  render() {
+       const addUser = () =>{
+         const user = faker.helpers.createCard();
+         const addedUser = this.state.users.unshift(user)
+         this.setState(
+             {
+                 users: addedUser,
+             }
+         )
+       }
+    const updateUsers = (arr, target) => {
+      let newUsers = arr.map((ele, idx) => {
+        if (idx === target) {
+          ele.isFavorite = !ele.isFavorite;
+        }
+        return ele;
+      });
+      let count = newUsers.filter( ele => {
+          return ele.isFavorite;
+      }).length;
+      this.setState({
+          users: newUsers,
+          favUsers: count,
+      })
+    };
+
+    return (
+      <div className = 'container'>
+          <h1>Acme Faker Favorites</h1>
+          <h2 className='underlined' onClick={addUser}>You have {this.state.favUsers} favorite users!</h2>
+        {this.state.users.map((obj, idx) => {
+          return (
+            <div className="user"
+              onClick={ev => {
+                this.state.users[idx].isFavorite
+                  ? ev.target.classList.remove('favorite')
+                  : ev.target.classList.add('favorite');
+                  updateUsers(this.state.users, idx);
+              }}
+              key={
+                idx
+              }
+            >
+              {obj.name}<br/>
+              {obj.username} 
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<List />, document.getElementById('app'));
